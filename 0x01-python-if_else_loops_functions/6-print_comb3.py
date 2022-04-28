f13dd7fd6dc79bd4c959b40a6c4062b545a6c552
yamilavila <yamilpra@hotmail.com>
2022-04-28 00:39:33 +0000

    play with operators

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
-for numbr1 in range(10):
-    for numbr2 in range(numbr1, 10):
-        if numbr1 > numbr2:
-            print("{:d}{:d}".format(numbr1, numbr2), end='')
-            if numbr1 * 10 + numbr2 == 89:
-                print('')
-            else:
-                print(', ', end='')
+for numbr1 in range(0, 9):
+    for numbr2 in range(0, 10):
+        if numbr1 < numbr2 and numbr1 != 8:
+            print("{:d}{:d}, ".format(numbr1, numbr2), end='')
+        if numbr1 < numbr2 and numbr1 == 8:
+            print("{:d}{:d},".format(numbr1, numbr2))
