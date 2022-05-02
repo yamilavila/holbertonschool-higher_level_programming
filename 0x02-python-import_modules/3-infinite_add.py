8f6dd62004993d62e473ec2ef722c42b5510ffc3
yamilavila <yamilpra@hotmail.com>
2022-05-02 13:52:45 +0000

    Fix checkers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
     sum = 0
+    from sys import argv
     for i in range(1, len(argv)):
-        sum += int(argv[1])
+        sum += int(argv[i])
         print("{:d}".format(sum))
