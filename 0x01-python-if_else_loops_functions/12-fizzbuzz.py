27dc3c2c700be5ce10ac668595fbcb6e5f40a73e
yamilavila <yamilpra@hotmail.com>
2022-05-03 09:48:34 +0000

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for a in range(1, 100 + 1):
+        if a % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif a % 3 == 0:
+            print("Fizz", end=" ")
+        elif a % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(a, end=" ")
+
