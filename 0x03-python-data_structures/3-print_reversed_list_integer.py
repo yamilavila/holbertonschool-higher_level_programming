c50b739d829862560de7d702b92ac31cf12077d0
yamilavila <yamilpra@hotmail.com>
2022-05-05 02:35:58 +0000

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        for my_list in my_list[::-1]:
-            print("{:d}".format(my_list))
+if __name__ == "__main__":
+    sum = 0
+    from sys import argv
+    for a in range(1, len(argv)):
+        sum += int(argv[a])
+    print("{d}".format(sum))
