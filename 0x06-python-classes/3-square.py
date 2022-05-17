ba69a55638773defd5f780d6e99ef637b5dd5e2a
yamilavila <yamilpra@hotmail.com>
2022-05-17 15:32:13 +0000

    Task 3, Square Area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 3:
+    Area of a square"""
+
+class Square:
+    """
+    Class that defines a square that returns the 
+    current square area"""
+    
+    def __init__(self, size=0):
+        self.__size = size
+    """This methot initialize the class"""
+
+    def area(self):
+        """THis other method is for calculation of the area square"""
+        self.area = self.__size * self.__size
+        return self.area
