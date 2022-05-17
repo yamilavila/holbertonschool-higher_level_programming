ea95a36179255f3e53866de15e81a8b766cd4db4
yamilavila <yamilpra@hotmail.com>
2022-05-17 02:42:39 +0000

    adv task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!usr/bin/python3
 import sys
+
 def safe_function(fct, *args):
     reslt = None
     try:
         reslt = fct(*args)
     except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception:", sys.exc_info()[1], file=sys.stderr)
     return reslt
