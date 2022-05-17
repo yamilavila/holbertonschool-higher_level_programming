7ba87e2011b1de8da4ddb5e06af9223fbdc0957c
yamilavila <yamilpra@hotmail.com>
2022-05-17 14:48:32 +0000

    Task 1 Size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Task 1
+Define a square"""
+
+class Square:
+    """set up a new square"""
+    def __init__(self, size):
+        """args is
+        size: size for the new instance attribute(square)"""
+        self.__size = size
