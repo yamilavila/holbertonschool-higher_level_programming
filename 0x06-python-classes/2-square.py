aa508b6ce891518801533a28aa1cf245ef2a0d8a
yamilavila <yamilpra@hotmail.com>
2022-05-19 01:52:51 +0000

    Fck indentation error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Task 2
+Size validation"""
+
+
+class Square:
+    """Defines a square by:
+        args size - size: def __init__(self, size=0)"""
+
+    def __init__(self, size=0):
+            """setting up the square"""
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
