1b5b009fa8fb00fbed19a56f30bc203e9410d5a9
yamilavila <yamilpra@hotmail.com>
2022-05-26 01:29:03 +0000

    Task 1, height & width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Task 1:
+Real definition of a rectangle
+"""
+
+
+class Rectangle:
+    """Define a rectangle"""
+    def __init__(self, width=0, height=0):
+        """attribute init"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """method property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the value of the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """method property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
