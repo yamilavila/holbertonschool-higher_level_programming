c2e57bc0fbe8ebebd03833916a2691870cce394a
yamilavila <git@github.com>
2022-06-24 14:46:33 +0000

    Bug fixed, E113

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,9 @@ class Square:
         args size - size: def __init__(self, size=0)"""
 
     def __init__(self, size=0):
-            """setting up the square"""
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
+        """setting up the square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
             if size < 0:
                 raise ValueError("size must be >= 0")
             self.__size = size
