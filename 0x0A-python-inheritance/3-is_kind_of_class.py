a6dd9c3df495d312b0aabb181faa2d0eba48e66d
yamilavila <git@github.com>
2022-06-30 16:45:42 +0000

    Task 3,isinstance type, built-in funct for inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task 3: Same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that returns True if the object is an instance of, or
+    if the object is an instance of a class that inherited from, the
+    specified class; otherwise False.
+    """
+    return isinstance(obj, a_class)
