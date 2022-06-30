cd7bcc922f61fe9ceb14b394f0c446ddfcaf2ea6
yamilavila <git@github.com>
2022-06-30 20:12:45 +0000

    Task 7, int validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Task 7, Integer validator
+"""
+
+
+class BaseGeometry():
+    """
+    Geometry basics
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
