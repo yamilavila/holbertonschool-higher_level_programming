b286ad92d93a12ab8438be2dd96d7db5ce78db76
yamilavila <git@github.com>
2022-06-30 18:51:55 +0000

    Task 6, Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+task 6. Improve Geometry
+"""
+
+
+class BaseGeometry():
+    """
+    Empty class
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
