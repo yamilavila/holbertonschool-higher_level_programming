5bcb910318718f18f81ba6dffb28a20aee9f1707
yamilavila <git@github.com>
2022-07-01 19:59:23 +0000

    Task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,9 +7,13 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
+    Rectangle that inherits from BaseGeometry
     """
     def __init__(self, width, height):
         """
+        Initializer with the args:
+        width and height must be private.
+        width and height must be positive int validated by integer_validator
         """
         BaseGeometry.integer_validator(self, 'height', height)
         self.__height = height
@@ -18,10 +22,14 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         """
+        Method:
+        int: area > 0
         """
         return self.__width * self.__height
 
     def __str__(self):
         """
+        Build in funct for:
+        string representation
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
