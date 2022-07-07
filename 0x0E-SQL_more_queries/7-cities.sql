1f21dea6b0b2635e19e6b93f8a86c2c89913d5f3
yamilavila <git@github.com>
2022-07-07 19:57:15 +0000

    Cities table with 2 keys: primary & foreign

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- Task 7.Cities table
+-- creates the database hbtn_0d_usa and the table cities
+-- Description: id INT unique, auto generated, can`t be null and is primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can`t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+	);
