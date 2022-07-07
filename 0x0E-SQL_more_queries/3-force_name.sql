658f4424b06835c48c76cd655b38f0b8ff08ab76
yamilavila <git@github.com>
2022-07-07 18:55:22 +0000

    New table, force_name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Task 3.Always a name
+-- script that creates the table force_name
+-- Description: id INT, name VARCHAR(256) can`t be NULL
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
+	);
