87c17bb098c68345611c3e1f3db583dfe5bf0612
yamilavila <git@github.com>
2022-07-07 23:51:14 +0000

    Importing without genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,12 @@
+-- Task 12.No genre
+-- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked. 
+-- Description:
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_shows_genres
+ON tv_shows.id = tv_shows_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
