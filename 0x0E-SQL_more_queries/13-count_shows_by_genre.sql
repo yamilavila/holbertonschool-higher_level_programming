bc819ac910e864e1f72d2bf464d34c90b4d80985
yamilavila <git@github.com>
2022-07-08 00:37:50 +0000

    bug fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,5 @@
 -- Task 13.Number of shows by genre
--- Description:
--- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
--- First column must be called genre
--- Second column must be called number_of_shows
--- must be sorted in descending order by the number of shows linked
--- one SELECT statement
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre
 SELECT 
 tv_genres.name AS genre,
 COUNT(tv_show_genres.genre_id) AS number_of_shows
