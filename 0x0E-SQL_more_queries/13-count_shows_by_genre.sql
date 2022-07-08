07579ee55d13c1cea7f9b6c2951fc97c2103233c
yamilavila <git@github.com>
2022-07-08 00:02:52 +0000

    more importing shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,13 @@
+-- Task 13.Number of shows by genre
+-- Description:
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- Second column must be called number_of_shows
+-- must be sorted in descending order by the number of shows linked
+-- one SELECT statement
+SELECT tv_genres.name AS "genre", COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_shows_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
