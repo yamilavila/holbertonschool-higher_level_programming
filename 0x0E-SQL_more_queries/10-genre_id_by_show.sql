03a9baa675aa4d6a0f4c66ad6f94f8d0894826da
yamilavila <git@github.com>
2022-07-08 00:25:24 +0000

    id setting

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,9 @@
 -- Task 10.Genre ID by show
 -- script that lists all shows contained in hbtn_0d_tvshows
 -- Description: Each record should display: tv_shows.title - tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre.id
+SELECT 
+tv_shows.title, 
+tv_show_genres.genre.id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
