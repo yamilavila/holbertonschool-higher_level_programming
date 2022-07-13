140e8fe1d4773d3f9471a93372cc34dd007eac6a
yamilavila <git@github.com>
2022-07-13 02:00:23 +0000

    Task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -15,13 +15,14 @@ class Rectangle:
         """Prints a rectangle with #"""
         if self.area() == 0:
             return ""
-        
+
         return (('#' * self.__width + '\n') * self.__height)[:-1]
 
     def __repr__(self):
 
         """String representation"""
-        return "{}({}, {})".format(self.__class__.__name__, self.width, self.height)
+        return "{}({}, {})".format(self.__class__.__name__,
+                                   self.width, self.height)
 
     @property
     def width(self):
@@ -31,32 +32,32 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """sets the value of the width"""
-        if type(value) is not int:                                                            
-            raise TypeError("width must be an integer")                                       
-        if value < 0:                                                                         
-            raise ValueError("width must be >= 0")                                            
-        self.__width = value                                                                  
-                                                                                              
-    @property                                                                                 
-    def height(self):                                                                         
-        """property method"""                                                                  
-        return self.__height                                                                  
-                                                                                              
-    @height.setter                                                                            
-    def height(self, value):                                                                  
-        """sets the height of the value"""                                                    
-        if type(value) is not int:                                                            
-            raise TypeError("height must be an integer")                                      
-        if value < 0:                                                                         
-            raise ValueError("height must be >= 0")                                           
-        self.__height = value                                                                 
-                                                                                              
-    def area(self):                                                                           
-        """calculation of the area"""                                                         
-        return self.__height * self.__width                                                   
-                                                                                              
-    def perimeter(self):                                                                      
-        """calculation of the perimeter"""                                                    
-        if self.width == 0 or self.height == 0:                                               
-            return 0                                                                          
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """property method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height of the value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculation of the area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """calculation of the perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
         return 2 * (self.__height + self.__width)
