d7c18cad1e0963c3df7fda032ee423f30a7f0363
yamilavila <git@github.com>
2022-08-01 21:54:21 +0000

    Task 6.Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let ml = 0; ml < multiLanguages.length; ml++) {
+  console.log(multiLanguages[ml]);
+}
