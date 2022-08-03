2580d91ef85852d5ef534cd021bc1310766f3e70
yamilavila <git@github.com>
2022-08-03 00:31:48 +0000

    Adv task12.Sorted ocurrences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+const newdict = {};
+for (const key in dict) {
+  if (dict[key] in newdict) {
+    newdict[dict[key]].push(key);
+  } else {
+    newdict[dict[key]] = [key];
+  }
+}
+console.log(newdict);
