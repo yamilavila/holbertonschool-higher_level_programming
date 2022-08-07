d77c6e353e8b96add09c08a485d5560251f78af7
yamilavila <git@github.com>
2022-08-07 16:49:54 +0000

    Task 1.Filter states, line 10 index out of range

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,10 +6,10 @@ import MySQLdb
 
 if __name__ == '__main__':
     database = MySQLdb.connect(host="localhost",
-            port=3306,
-            user=argv[1],
-            password=argv[2],
-            db=argv[3])
+                               port=3306,
+                               user=argv[1],
+                               password=argv[2],
+                               db=argv[3])
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
                     ORDER BY id ASC")
