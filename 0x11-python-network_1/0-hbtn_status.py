778beedef63b8c933a75821c064d2821abe8f104
yamilavila <git@github.com>
2022-08-19 20:46:38 +0000

    Adding file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ Python script that fetches https://intranet.hbtn.io/status
 
 import urllib.request
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         body = response.read()
         print("Body response:")
