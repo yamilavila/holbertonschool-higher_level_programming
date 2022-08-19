266bf1091734363f399b4d4afe2e68176683ccd7
yamilavila <git@github.com>
2022-08-19 20:32:12 +0000

    Adding file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(html))
+        print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
