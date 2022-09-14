26d1b2e7c0e1c083e8f437b2fa32baaf2add4503
yamilavila <yamilpra@hotmail.com>
2022-09-14 10:28:50 -0400

    Task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
