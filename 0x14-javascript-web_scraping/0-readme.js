ef0c3b48df9ba586278add3bbf20f27547290299
yamilavila <yamilpra@hotmail.com>
2022-09-13 21:08:28 -0400

    Adding file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const fl = process.argv[2];
+const file = process.argv[2];
 
-fs.readFl(fl, 'utf8', error);
+fs.readFile(file, 'utf8', error);
 
 function error (bad, data) {
   if (bad) {
-    console.log(bad);
+   console.log(bad);
   } else {
     process.stdout.write(data);
   }
